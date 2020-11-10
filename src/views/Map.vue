4695ccb56063ea45a5ef94537027d404e7083222
dupp <dupp@sucsoft.com>
2020-11-10 09:52:28 +0800

    fetch files from tswebdemo

---
+++ src/views/Map.vue
@@ -0,0 +1,20 @@
+<template>
+    <suc-map ref="mapObject" :options="olMap.map" v-bind.sync="olMap.view">
+        <!--控件-->
+        <template v-for="item in olMap.controls">
+            <ol-control :name="item.name" :config="item.config"></ol-control>
+        </template>
+        <!--基础底图-->
+        <template v-for="layer in olMap.baseLayer">
+            <ol-layer :options="layer"></ol-layer>
+        </template>
+    </suc-map>
+</template>
+<script lang="ts">
+    import { Vue, Component } from "vue-property-decorator";
+
+    @Component
+    export default class MapView extends Vue {
+        olMap: any = this.$getMapConfig();
+    }
+</script>
